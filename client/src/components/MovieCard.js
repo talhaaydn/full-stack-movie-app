9e58122cfb44ce463a23d919a0a9451396339efc
Talha AYDIN <talhaydn58@gmail.com>
2019-08-07 14:35:15 +0300

    added edit and delete buttons

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { Card, Grid } from 'semantic-ui-react'
+import { Card, Grid, Button, Icon } from 'semantic-ui-react'
+
+const extra = (
+  <div className="ui two buttons">
+    <Button animated>
+      <Button.Content visible>Edit</Button.Content>
+      <Button.Content hidden>
+        <Icon name='edit' />
+      </Button.Content>
+    </Button>
+    <Button animated="vertical">
+      <Button.Content visible>Delete</Button.Content>
+      <Button.Content hidden>
+        <Icon name='trash' />
+      </Button.Content>
+    </Button>
+  </div>
+)
 
 const MovieCard = (props) => (
   <Grid.Column>
     <Card
       image={props.movie.cover}
       header={props.movie.title}
+      extra={extra}
     />
   </Grid.Column>
   
